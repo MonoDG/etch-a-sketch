275160486d67a6446f369ebf64452a89f97d4b22
MonoDG <monodg@proton.me>
2023-09-21 12:55:47 -0400

    Add button to recreate grid with given width input

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
-function createSquareDiv() {
+function createSquareDiv(width) {
     const squareDiv = document.createElement("div")
     squareDiv.classList.add("squared");
+    let newWidth = Math.round((960 / width) * 100) / 100;
+    squareDiv.style.width = `${newWidth}px`;
+    squareDiv.style.height = `${newWidth}px`;
     return squareDiv;
 }
 
 function createGrid(width) {
     const gridContainer = document.querySelector(".grid-container");
-    for (let i = 0; i < width; i++) {
-        const squareDiv = createSquareDiv();
+    for (let i = 0; i < width * width; i++) {
+        const squareDiv = createSquareDiv(width);
         squareDiv.addEventListener("mouseover", applyHoverColor);
         gridContainer.appendChild(squareDiv);
     }
@@ -22,4 +25,16 @@ function clearGrid() {
     squares.forEach(square => square.classList.remove("hovered"));
 }
 
-createGrid(256);
+function recreateGrid() {
+    let newWidth = parseInt(prompt("How many squares per side (max 100): "));
+    if (!Number.isInteger(newWidth)) {
+        newWidth = 16;
+    }
+    const gridContainer = document.querySelector(".grid-container");
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    createGrid(newWidth);
+}
+
+createGrid(16);
